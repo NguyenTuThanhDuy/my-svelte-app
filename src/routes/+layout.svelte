e30be86eae1c175fc035a960e1ad70d3f4bb44d7
Duy Nguyen <duyntt0110@gmail.com>
2024-09-06 16:13:05 +0700

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import '../app.css';
+	import Footer from './Footer.svelte';
+	import Navbar from './Navbar.svelte';
+</script>
+
+<Navbar />
+<div>
+	<slot></slot>
+</div>
+<Footer />
---
+++ src/routes/Navbar.svelte
@@ -0,0 +1,64 @@
+<script>
+	import { page } from '$app/stores';
+	import { AppBar, Avatar } from '@skeletonlabs/skeleton';
+
+	$: routeId = $page.route.id;
+
+	const items = [
+		{
+			icon: 'mdi-home',
+			title: 'Home',
+			value: 'home',
+			route: '/'
+		},
+		{
+			icon: 'mdi-account',
+			title: 'Login',
+			value: 'login',
+			route: '/login'
+		},
+		{
+			icon: 'mdi-contacts',
+			title: 'Sign Up',
+			value: 'signup',
+			route: '/signup'
+		}
+	];
+</script>
+
+<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+	<svelte:fragment slot="lead">
+		<h1 class="h1">
+			<span
+				class="bg-gradient-to-br from-red-500 to-yellow-500 box-decoration-clone bg-clip-text text-transparent"
+				>Build.</span
+			>
+		</h1>
+	</svelte:fragment>
+	<svelte:fragment slot="trail">
+		{#each items as i}
+			<li>
+				<button type="button" class="btn variant-filled">
+					<a href={i.route} class:active={routeId == i.route} data-sveltekit-preload-data="hover"
+						>{i.title}</a
+					>
+				</button>
+			</li>
+		{/each}
+		<Avatar
+			border="border-4 border-surface-300-600-token hover:!border-primary-500"
+			cursor="cursor-pointer"
+		/>
+	</svelte:fragment>
+</AppBar>
+
+<style>
+	a {
+		text-decoration: none;
+		color: white;
+	}
+
+	.active {
+		color: red;
+	}
+</style>
