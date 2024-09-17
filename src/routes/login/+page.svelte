936fbc3c84d00394e74328f75f2fa282279df77c
Duy Nguyen <duyntt0110@gmail.com>
2024-09-17 16:07:11 +0700

    cmt

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -2,17 +2,21 @@
 	import Login from 'svelte-material-icons/Login.svelte';
 	import axiosInstance from '../../services/axios';
 
+	let isSubmit = false;
+
 	const handleSubmit = async (e: SubmitEvent) => {
 		e.preventDefault();
 		const formData = new FormData(e.target as HTMLFormElement);
 		try {
-			const response = await axiosInstance.post('http://localhost:8000/api/v1/login', {
-				email: formData.get('email'),
+			isSubmit = true;
+			const response = await axiosInstance.post(`${import.meta.env.VITE_API_URL}login`, {
+				username: formData.get('email'),
 				password: formData.get('password')
 			});
 			console.log(response);
 		} catch {
 			console.error('Error');
+			isSubmit = false;
 		}
 	};
 </script>
@@ -24,11 +28,11 @@
 			SnowWhite
 		</a>
 		<div
-			class="w-full rounded-lg bg-white shadow sm:max-w-md md:mt-0 xl:p-0 dark:border dark:border-gray-700 dark:bg-gray-800"
+			class="w-full rounded-lg bg-white shadow dark:border dark:border-gray-700 dark:bg-gray-800 sm:max-w-md md:mt-0 xl:p-0"
 		>
 			<div class="space-y-4 p-6 sm:p-8 md:space-y-6">
 				<h1
-					class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white"
+					class="text-xl font-bold leading-tight tracking-tight text-gray-900 dark:text-white md:text-2xl"
 				>
 					Sign in to your account
 				</h1>
@@ -46,7 +50,7 @@
 							type="email"
 							name="email"
 							id="email"
-							class="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+							class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 focus:border-primary-600 focus:ring-primary-600 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
 							placeholder="name@company.com"
 							required={true}
 						/>
@@ -61,7 +65,7 @@
 							name="password"
 							id="password"
 							placeholder="••••••••"
-							class="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+							class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 focus:border-primary-600 focus:ring-primary-600 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
 							required={true}
 						/>
 					</div>
@@ -72,7 +76,7 @@
 									id="remember"
 									aria-describedby="remember"
 									type="checkbox"
-									class="focus:ring-3 focus:ring-primary-300 dark:focus:ring-primary-600 h-4 w-4 rounded border border-gray-300 bg-gray-50 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800"
+									class="focus:ring-3 h-4 w-4 rounded border border-gray-300 bg-gray-50 focus:ring-primary-300 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-primary-600"
 									required={false}
 								/>
 							</div>
@@ -82,19 +86,19 @@
 						</div>
 						<a
 							href="/forget"
-							class="text-primary-600 dark:text-primary-500 text-sm font-medium hover:underline"
+							class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
 							>Forgot password?</a
 						>
 					</div>
 					<button
 						type="submit"
-						class="bg-primary-600 hover:bg-primary-700 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 w-full rounded-lg px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4"
-						>Sign in</button
+						class="w-full rounded-lg bg-primary-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-primary-700 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+						disabled={isSubmit}>Sign in</button
 					>
 					<p class="text-sm font-light text-gray-500 dark:text-gray-400">
 						Don’t have an account yet? <a
 							href="/signup"
-							class="text-primary-600 dark:text-primary-500 font-medium hover:underline">Sign up</a
+							class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a
 						>
 					</p>
 				</form>
